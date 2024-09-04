834ada803ed23a3993c59a48f3bbaf5ddfab2ec2
mh-mahdi-13 <munawarhossainmahdi@gmail.com>
2024-09-04 12:03:55 +0600

    first commit

---
+++ hello.js
@@ -0,0 +1,121 @@
+/*
+
+console.log("Hello World");
+
+var strType = "Mahdi";
+console.log(typeof strType)
+
+var numType = 20;
+console.log(typeof numType)
+
+var BoolType = true ;
+console.log(typeof BoolType)
+
+
+var mahdi ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium sit, pariatur possimus doloribus nulla officiis iure. Dolores beatae incidunt officia provident fugit? Laudantium id quo hic, ducimus adipisci repudiandae inventore."
+console.log(mahdi.toLocaleLowerCase())
+*/
+
+//var myFd = "Eshan Tutul Hasibul Tuhin"
+//console.log(myFd.split("Tutul"));
+
+//var myFd = "Eshan Tutul Hasibul Tuhin"
+//console.log(myFd.indexOf("Tuhin"));
+
+// var num1 = "55";
+// var num2 = 126;
+
+// //num1= parseFloat(num1);
+
+// var sum = num1 + num2
+// console.log(sum)
+
+
+// var a = 5;
+// var b = 5;
+
+// if (a > b){
+//     console.log("True");
+// }
+// else if(a < b) {
+//      console.log("False");
+// }
+// else{
+//     console.log("They both are same");
+// }
+
+
+// var sum = 6;
+
+// switch(sum){
+//     case 5: 
+//     console.log("hello");
+//     break;
+
+//     case 1: 
+//     console.log("hi");
+//     break;
+
+//     case 3: 
+//     console.log("now");
+//     break;
+
+//     default:
+//         console.log("bye");
+// }
+
+
+// var myDate = new Date ();
+
+// console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toDateString());
+// console.log(myDate.getDate());
+
+
+// var fdName = ["x", "y", "z"]
+// fdName[2] = "mahdi"
+// console.log(fdName)
+
+
+// var fdName = ["x", "y","z"]
+// // console.log(fdName[2])
+// // fdName.push("mahdi")
+// // fdName.pop()
+// // fdName.unshift("mahdi")
+// // fdName.shift()
+// // fdName.slice(2,3)
+// fdName.splice(0,2, "mahdi")
+// console.log(fdName)
+
+
+// for(var sum=1 ; sum<5 ; sum++ ){
+//     console.log("Mahdi")
+// }
+//  var i=0;
+// while(i<20){
+//     console.log("Mahdi")
+//     i++
+// }
+
+// function mad(num1,num2){
+//     var total=num1+num2
+//     console.log(total)
+// }
+// mad(20,50)
+
+// var num= 5;
+
+// var myCon=(num>10) ? "Mahdi" : "No One" ;
+// console.log(myCon)
+
+for(nota=0 ; nota<10 ; nota++){
+
+    if(nota == 3){
+        console.log("Hims ka bins");
+        continue;
+    }
+    console.log(nota);
+}
+
+console.log(object);
---
+++ script.js
@@ -0,0 +1,72 @@
+// // // ১. Variable কি ?
+
+// // // ২. Variable কিভাবে লিখতে হয় ?
+
+// = var num = 1;
+
+
+
+// // // ৩. string type variable কি ও কি ভাবে লিখতে হয় ?
+// "", ``, '' এইগুলা দিয়ে কোন variable লিখলে তাকে String Type বলে । 
+// Ex- var num = "1";
+
+
+
+// // // ৪. number type variable কি ও কি ভাবে লিখতে হয় ?
+// "", ``, '' এইগুলা ছাড়া কোন সংখ্যা দিয়ে variable লিখলে তাকে Number Type বলে ।
+// Ex- var num = 20;
+
+
+
+// // // ৫. Boolan type variable  কি ও কি ভাবে লিখতে হয় ?
+
+// যেসকল var এর উত্তর True বা False এর মধ্যে সিমাবদ্ধ তাকে Boolean Type বলে ।
+
+// Ex- var Boolean = true ;
+
+
+// // // ৬. toUpperCase() & toLowerCase() এর ব্যাবহার কি ভাবে করতে হয় ? 
+
+// সব লিখাকে বড় হাতের করার জন্য toUpperCase() আর সব লিখাকে ছোট হাতের করার জন্য toLowerCase() ব্যবহার করা হয়।
+
+// Ex-
+
+// // // ৭. JavaScript এর মোট কয়টি অপারেটর আছে ও কি কি ?
+
+// ৫ টি । যথা ঃ + - * / %
+
+// Ex- 
+    var num1 = 5;
+    var num2 = 3;
+    console.log(num1 + num2) 
+    console.log(num1 - num2) 
+    console.log(num1 * num2) 
+    console.log(num1 / num2) 
+    console.log(num1 % num2) 
+
+// ৮. Math.abs() এর ব্যাবহার লিখুন । 
+
+var Sum = Math.abs(num1)
+console.log(Sum);
+
+// ৯. Math.celi()  এর ব্যাবহার লিখুন । 
+
+var num3 = 1.02
+var total = Math.ceil(num3)
+console.log(total);
+// ১০. Math.Floor() এর ব্যাবহার লিখুন । 
+
+var total = Math.floor(num3)
+console.log(total);
+
+// ১১. Math.round() এর ব্যাবহার লিখুন ।
+
+var total = Math.round(num3)
+console.log(total);
+
+// ১২. Math.random() এর ব্যাবহার লিখুন ।
+
+var total = Math.random(num2)* 1000
+console.log(total);
+ 
+// asignment.js - GitHub Link 
